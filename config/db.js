6e43ecfa92106e243d176e915eeac16b5d5b2e35
hoangtd21 <tranduchoang21399@gmail.com>
2020-12-21 10:11:11 +0700

    update databaseURL

--- config/db.js
+++ config/db.js
@@ -2,6 +2,6 @@ const mongose = require('mongoose');
 
 mongose.Promise = global.Promise;
 
-mongose.connect('mongodb+srv://hoangtd:hoangtd21@pusherpoll.vpnci.mongodb.net/pusherpoll?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology: true})  
+mongose.connect(process.env.DATABASE_URL,{useNewUrlParser: true, useUnifiedTopology: true})  
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
